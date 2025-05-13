48c8fceaf438544eafa612fd3b65c307d7429a57
ptyytty <typark@gmail.com>
2025-05-13 18:35:12 +0900

    db test

--- Assets/Scripts/Manager/DBManager.cs
+++ Assets/Scripts/Manager/DBManager.cs
@@ -8,7 +8,7 @@ using UnityEngine.Networking;
 public class DBManager : MonoBehaviour
 {
     //58.230.61.213 <- 공인 IP
-    private string url = "http://localhost:3000/";
+    private string url = "http://58.230.61.213:3000/";
 
     //싱글톤
     public static DBManager instance = null;
@@ -45,13 +45,16 @@ public class DBManager : MonoBehaviour
     //job 테이블 호출 코루틴
      public IEnumerator GetJobDataFromDatabase(){
         UnityWebRequest request = UnityWebRequest.Get(url + "jobs");
+        
 
         // 요청을 보내고 기다리기
         yield return request.SendWebRequest();
+        Debug.Log("Start Request");
 
         // 요청이 성공적으로 완료되었을 경우
         if (request.result == UnityWebRequest.Result.Success)
         {
+            Debug.Log("Connect Succes");
             string json = request.downloadHandler.text;
 
             //json이 전체 배열일 때 변환
